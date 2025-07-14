afa92630fa9723fff03963e71487741b1232a040
NotPandaByte <cloakzery@gmail.com>
2025-07-14 05:30:44 +0200

    meow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,3 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "src/generated/**/*",
-      "node_modules/**/*",
-      ".next/**/*",
-      "prisma/generated/**/*",
-      "**/*.js",
-      "**/*.mjs"
-    ],
-    rules: {
-      "@next/next/no-img-element": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "@typescript-eslint/no-require-imports": "off",
-      "@typescript-eslint/no-explicit-any": "warn"
-    }
-  },
+export default [
+  "next"
 ];
-
-export default eslintConfig;
