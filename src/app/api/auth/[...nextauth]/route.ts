45adf80438498f2bb85a859f5fe6f0423643bdec
NotPandaByte <cloakzery@gmail.com>
2025-07-14 17:14:22 +0200

    meow

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import type { Session, User } from "next-auth";
 
 const prisma = new PrismaClient();
 
-export const authOptions = {
+const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
